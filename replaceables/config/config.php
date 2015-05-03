614ea1d2558fe1648105fb52395aca200becb5b3
François Hodierne <francois@hodierne.net>
2015-05-03 22:33:17 +0200

    amateur -> blogmarks

--- replaceables/config/config.php
+++ replaceables/config/config.php
@@ -2,13 +2,13 @@
 
 function config($name, $default = null, $value = null)
 {
-  if (!isset(amateur::$registry['config'])) {
-    amateur::$registry['config'] = [];
+  if (!isset(blogmarks::$registry['config'])) {
+    blogmarks::$registry['config'] = [];
   }
   if ($value) {
-    return amateur::$registry['config'][$name] = $value;
+    return blogmarks::$registry['config'][$name] = $value;
   }
   else {
-    return isset(amateur::$registry['config'][$name]) ? amateur::$registry['config'][$name] : $default;
+    return isset(blogmarks::$registry['config'][$name]) ? blogmarks::$registry['config'][$name] : $default;
   }
 }
