cbf4e8e00aed06bdc1336e7add57f97c6c11af50
François Hodierne <francois@hodierne.net>
2015-05-22 09:48:06 +0200

    make backup compression optionnal

--- application/renders/marks.render.php
+++ application/renders/marks.render.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 
 $request_format = request_format();
 
@@ -14,9 +14,15 @@ elseif ($request_format == 'atom') {
   response_header('Content-Type', 'application/atom+xml; charset=utf-8');
   view('marks/atom');
   if (get_param('export')) {
-    response_header('Content-Type', 'application/x-gzip');
-    response_header('Content-Disposition', 'attachment; filename="bm3-backup.atom.xml.gz"');
-    response_content(gzencode(response_content()));
+    if (flag('compress_export')) {
+      response_header('Content-Type', 'application/x-gzip');
+      response_header('Content-Disposition', 'attachment; filename="bm3-backup.atom.xml.gz"');
+      response_content(gzencode(response_content()));
+    }
+    else {
+      response_header('Content-Type', 'application/x-download');
+      response_header('Content-Disposition', 'attachment; filename="bm3-backup.atom.xml"');
+    }
   }
 }
 elseif (request_param('more-marks')) {
