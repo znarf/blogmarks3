de57ccf30dac43ac9fddc5ed29e391e318ee886f
François Hodierne <francois@hodierne.net>
2023-04-03 22:33:19 +0200

    update css setup

--- public/style/bm.css.php
+++ public/style/bm.css.php
@@ -1,29 +1,29 @@
 <?php
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../bootstrap.php';
 
 $css_files = array(
-    "p-init.less",
-    "p-extra.less",
-    "p-bootstrap.less",
-    "p-title-bar.less",
-    "p-content.less",
-    "p-markers.less",
-    "p-marks.less",
-    "p-pagination.less",
-    "p-right-bar.less",
-    "p-taglist.less",
-    "p-userlist.less",
-    "p-footer.less",
-    "p-tools.less",
-    "p-new.less",
-    "p-responsive.less"
+  "p-init.less",
+  "p-extra.less",
+  "p-bootstrap.less",
+  "p-title-bar.less",
+  "p-content.less",
+  "p-markers.less",
+  "p-marks.less",
+  "p-pagination.less",
+  "p-right-bar.less",
+  "p-taglist.less",
+  "p-userlist.less",
+  "p-footer.less",
+  "p-tools.less",
+  "p-new.less",
+  "p-responsive.less"
 );
 
 $merged = "";
 foreach ($css_files as $file) {
-    $file = __DIR__ . '/' . $file;
-    $merged .= file_get_contents($file) . "\n";
+  $file = __DIR__ . '/' . $file;
+  $merged .= file_get_contents($file) . "\n";
 }
 
 $less = new lessc();
