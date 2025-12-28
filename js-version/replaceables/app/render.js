260d4ca61ce858ed176fe9f3b038829fa87a76c7
François Hodierne <francois@hodierne.net>
2025-12-28 16:48:37 +0100

    update render

--- js-version/replaceables/app/render.js
+++ js-version/replaceables/app/render.js
@@ -1,22 +1,15 @@
 const fs = require('fs');
 
-function render(view, args = [], layout = 'default') {
-  const filename = blogmarks.filename('render', view);
+function render(viewName, args = {}, layoutName = 'default') {
+  const filename = replaceable('filename')('render', viewName);
   if (filename && fs.existsSync(filename)) {
-    const scoped = args;
-    const result = include(filename, scoped);
-    if (result !== undefined) {
-      return blogmarks.response_content(result);
-    }
+    replaceable('include')(filename, args);
   } else {
-    const content = blogmarks.view(view, args);
-    const layoutOutput = blogmarks.layout(layout);
-    return blogmarks.response_content(layoutOutput || content || '');
+    replaceable('view')(viewName, args);
+    replaceable('layout')(layoutName);
   }
-  if (blogmarks.registry.layout_output) {
-    return blogmarks.response_content(blogmarks.registry.layout_output);
-  }
-  blogmarks.finish();
+  replaceable('finish')();
+  return replaceable('response_content')();
 }
 
 module.exports = render;
